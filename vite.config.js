cfb059ed7dd8288c3e1b2cbce98d82132de5bfd9
manuel-gcs <122889932+manuel-gcs@users.noreply.github.com>
2025-11-26 14:56:10 +0000

    New commit

--- vite.config.js
+++ vite.config.js
@@ -2,15 +2,24 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react({
+      jsxRuntime: "automatic" // força React runtime moderno no Vercel
+    }),
+  ],
+
+  worker: {
+    // Corrige erro do Vercel com Web Workers
+    format: "es"
+  },
+
   build: {
+    sourcemap: false,
     rollupOptions: {
       output: {
+        // workers NÃO podem usar iife ou umd no Vercel
         format: "es"
       }
     }
-  },
-  worker: {
-    format: "es"
   }
 });
