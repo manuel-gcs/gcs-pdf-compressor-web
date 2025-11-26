216c18c0e46f3e6bd4dc9ee939dafec969c3a87f
manuel-gcs <122889932+manuel-gcs@users.noreply.github.com>
2025-11-26 15:53:47 +0000

    Use Vite React SWC and remove Babel config

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,9 @@ import react from "@vitejs/plugin-react-swc";
 export default defineConfig({
   plugins: [react()],
   build: {
-    target: "esnext",
-    sourcemap: false
+    target: "esnext"
+  },
+  worker: {
+    format: "es"
   }
 });
