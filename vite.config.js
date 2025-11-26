6e6607a9399e670839c73e4e37fdfb917485f49c
manuel-gcs <122889932+manuel-gcs@users.noreply.github.com>
2025-11-26 14:37:51 +0000

    Correcao Vercel

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,7 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: { target: "esnext" },
-  base: "/ghostscript-pdf-compress.wasm/",
-  worker: {format :'es'}
-});
+  base: './', // 🔥 ESSENCIAL PARA VERCEL
+})
